1adfb4ed5cbc641b6a7724d9f911e74e79cfed56
Raouf <acer@DESKTOP-HMQQE1N>
2021-01-21 20:37:27 +0330

    object clone

--- introreactapp/src/index.js
+++ introreactapp/src/index.js
@@ -1,7 +1,15 @@
-const address = {
-    street: '',
-    city: '',
-    country: ''
-}
+const first = [1, 2, 3];
+const second = [4, 5, 6];
 
-const {city: ci, country : ct, street: st} = address;
+var compined1 = first.concat(second);
+var compined2 = [...first, 'a', ...second, 'b'];
+
+console.log(compined1);
+console.log(compined2);
+
+const firstObject = {name: "Raouf"};
+const secondObject = {job : "Developer"};
+
+const combined3 = {...firstObject, ...secondObject};
+
+console.log(combined3);
