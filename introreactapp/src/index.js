7f53b5ce6363da3a1bd0cbf347eff93ee8acc6c3
Raouf <acer@DESKTOP-HMQQE1N>
2021-01-21 20:56:08 +0330

    class

--- introreactapp/src/index.js
+++ introreactapp/src/index.js
@@ -1,15 +1,12 @@
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+    
+    walk() {
+        console.log('walk');
+    }
+}
 
-var compined1 = first.concat(second);
-var compined2 = [...first, 'a', ...second, 'b'];
-
-console.log(compined1);
-console.log(compined2);
-
-const firstObject = {name: "Raouf"};
-const secondObject = {job : "Developer"};
-
-const combined3 = {...firstObject, ...secondObject};
-
-console.log(combined3);
+const person = new Person('Raouf');
+console.log(person);
