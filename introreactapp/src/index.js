b72798168c1af31827efda423382666a8857dcbc
Raouf <acer@DESKTOP-HMQQE1N>
2021-01-21 23:08:56 +0330

    modules

--- introreactapp/src/index.js
+++ introreactapp/src/index.js
@@ -1,23 +1,5 @@
-class Person {
-    constructor(name){
-        this.name = name;
-    }
-    
-    walk() {
-        console.log('walk');
-    }
-}
-
-class Teacher extends Person {
-    constructor(name, degree){
-        super(name);
-        this.degree = degree;
-    }
-    
-    teach(){
-        console.log('teach');
-    }
-}
+import { Person } from './person';
+import { Teacher } from './teacher';
 
 const person = new Person('Raouf');
 console.log(person);
