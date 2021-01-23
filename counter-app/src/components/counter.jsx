3f33fb4f6d6ae2628970c7d135b0fea42a5cbcd7
Raouf <acer@DESKTOP-HMQQE1N>
2021-01-23 23:41:36 +0330

    Added basic elements

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {};
+    render() { 
+        return (<React.Fragment><h1>Hello Wrold</h1>
+        <button>Increment</button></React.Fragment>);
+    }
+}
+
+export default Counter;
